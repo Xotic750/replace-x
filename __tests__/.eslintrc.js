af9dc4a0c7069976dc655700d3c6f812ac895098
Graham Fairweather <xotic750@gmail.com>
2019-07-20 17:50:30 +0200

    Alighn with project template

--- __tests__/.eslintrc.js
+++ __tests__/.eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
     'lodash/prefer-noop': 'off',
     'compat/compat': 'off',
     'prefer-rest-params': 'off',
-    'no-prototype-builtins': 'off',
-    'func-names': 'off',
+    'no-prototype-builtins': 'off'
   },
 };
