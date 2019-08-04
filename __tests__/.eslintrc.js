a4c970fcf9bd68c841679e8234238bef93e01ab4
Graham Fairweather <xotic750@gmail.com>
2019-08-04 18:04:51 +0200

    Auto build

--- __tests__/.eslintrc.js
+++ __tests__/.eslintrc.js
@@ -4,5 +4,10 @@ module.exports = {
     'no-void': 'off',
     'prefer-rest-params': 'off',
     'no-prototype-builtins': 'off',
+    'promise/catch-or-return': 'off',
+    'promise/always-return': 'off',
+    'promise/no-callback-in-promise': 'off',
+    'consistent-return': 'off',
+    'jest/no-test-return-statement': 'off',
   },
 };
